673b672d06e365556fe7298c8d54cccdf252918c
MariomartinezPastoreo <160287993+MariomartinezPastoreo@users.noreply.github.com>
2024-03-18 23:43:22 -0300

    Add files via upload

---
+++ ownable.py
@@ -0,0 +1,3 @@
+def set_owner(self, owner) :
+     set.owner = owner 
+     
---
+++ seller.py
@@ -0,0 +1,5 @@
+from user import User
+
+class Seller(User): 
+    def __init_(self, name):
+        super()._init_(name)
